7f1773565ef2e8766832f035f79ab5baf958e077
jyotiranjan1997 <manomohanty281@gmail.com>
2022-11-10 20:52:33 +0530

    day-03 file added

--- KFC-clone/src/Redux/store.js
+++ KFC-clone/src/Redux/store.js
@@ -1,9 +1,12 @@
 import {legacy_createStore,combineReducers,compose, applyMiddleware} from "redux"
 import thunk from "redux-thunk";
 import { productsReducer } from "./Products/procucts.reducer";
-import { authReducer } from "./Auth/auth.reducer";
+import { authReducer, OtpVerifier } from "./Auth/auth.reducer";
 
-const rootReducer = combineReducers({products:productsReducer,auth:authReducer});
+const rootReducer = combineReducers({
+  products: productsReducer,
+  auth: authReducer,
+  otpVerify: OtpVerifier});
 
 
 export const store= legacy_createStore(rootReducer,compose(applyMiddleware(thunk)));
