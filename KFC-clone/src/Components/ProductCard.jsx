f68c6f1d929eb6f18fe28bc896255f318d5dad44
Mohd Monish <97455677+Mohd2002Monish@users.noreply.github.com>
2022-11-10 11:07:37 -0500

    fw15_331-day3

--- KFC-clone/src/Components/ProductCard.jsx
+++ KFC-clone/src/Components/ProductCard.jsx
@@ -1,56 +1,82 @@
 import {
-    Flex,
-    Box,
-    Image,
-    Badge,
-    useColorModeValue,
-    Text,
-    HStack,
-    Button,
-  } from '@chakra-ui/react';
-  import { BsHeart, BsTriangleFill } from 'react-icons/bs';
-  import { FiShoppingCart } from 'react-icons/fi';
-  
-  
-  function ProductCard() {
-    return (
-      <Flex p={50} w="full" alignItems="center" justifyContent="center">
-        <Box
-          bg={useColorModeValue('white', 'gray.800')}
-          maxW="sm"
-          borderWidth="1px"
-          rounded="lg"
-          shadow="lg"
-          position="relative">
-            <Box
-              size="10px"
-              position="absolute"
-              top={3}
-              right={3}
-            > 
-            <BsHeart size={20} color='white' />    
-             </Box>
-          <Image
-            src={'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/L-8000197.jpg?ver=21.88'}
-            alt={`trial`}
-            roundedTop="lg"
-          />
-  
-          <Box p="6">
-            <Box align={'left'}><Text as="b">Wednesday Chicken Bucket</Text></Box>
-            
-            <HStack >
-                <Badge align="left" variant='outline' colorScheme='red' p={1}>
-                <BsTriangleFill/>
-              </Badge>
-              <Text fontSize={'sm'}>Non-veg</Text>
-            </HStack>
-            <Box align={'left'}><Text as="b">₹628.57</Text></Box>
-           <Button variant="solid" colorScheme="red" >Add to Cart <FiShoppingCart/> </Button> 
-          </Box>
+  Flex,
+  Box,
+  Image,
+  Badge,
+  useColorModeValue,
+  Text,
+  HStack,
+  Button,
+} from "@chakra-ui/react";
+import { BsHeart, BsTriangleFill } from "react-icons/bs";
+import { FiShoppingCart } from "react-icons/fi";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../Redux/cartRedux/cart.actions";
+
+function ProductCard() {
+  const dispatch = useDispatch();
+  return (
+    <Flex p={50} w="full" alignItems="center" justifyContent="center">
+      <Box
+        bg={useColorModeValue("white", "gray.800")}
+        maxW="sm"
+        borderWidth="1px"
+        rounded="lg"
+        shadow="lg"
+        position="relative"
+      >
+        <Box size="10px" position="absolute" top={3} right={3}>
+          <BsHeart size={20} color="white" />
         </Box>
-      </Flex>
-    );
-  }
-  
-  export default ProductCard;
+        <Image
+          src={
+            "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/L-8000197.jpg?ver=21.88"
+          }
+          alt={`trial`}
+          roundedTop="lg"
+        />
+
+        <Box p="6">
+          <Box align={"left"}>
+            <Text as="b">Wednesday Chicken Bucket</Text>
+          </Box>
+
+          <HStack>
+            <Badge align="left" variant="outline" colorScheme="red" p={1}>
+              <BsTriangleFill />
+            </Badge>
+            <Text fontSize={"sm"}>Non-veg</Text>
+          </HStack>
+          <Box align={"left"}>
+            <Text as="b">₹628.57</Text>
+          </Box>
+          <Button
+            onClick={() => {
+              dispatch(
+                addToCart({
+                  id: 1,
+                  img: "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/L-8000240.jpg",
+                  tital: "  The Allu Arjun Combo",
+                  cata: [
+                    "Hot & Crispy Chicken-1pc",
+                    "Grilled Chicken - Smoky Red -1 pc",
+                    "Chicken Popcorn-Regular",
+                    "Thousand Island",
+                    "Spicy Mix Fries -Medium",
+                  ],
+                })
+              );
+           
+            }}
+            variant="solid"
+            colorScheme="red"
+          >
+            Add to Cart <FiShoppingCart />{" "}
+          </Button>
+        </Box>
+      </Box>
+    </Flex>
+  );
+}
+
+export default ProductCard;
--- KFC-clone/src/Redux/store.js
+++ KFC-clone/src/Redux/store.js
@@ -1,9 +1,21 @@
-import {legacy_createStore,combineReducers,compose, applyMiddleware} from "redux"
+import {
+  legacy_createStore,
+  combineReducers,
+  compose,
+  applyMiddleware,
+} from "redux";
 import thunk from "redux-thunk";
 import { productsReducer } from "./Products/procucts.reducer";
 import { authReducer } from "./Auth/auth.reducer";
+import { cartReducer } from "./cartRedux/cart.Reducer";
 
-const rootReducer = combineReducers({products:productsReducer,auth:authReducer});
+const rootReducer = combineReducers({
+  products: productsReducer,
+  auth: authReducer,
+  cart: cartReducer,
+});
 
-
-export const store= legacy_createStore(rootReducer,compose(applyMiddleware(thunk)));
+export const store = legacy_createStore(
+  rootReducer,
+  compose(applyMiddleware(thunk))
+);
