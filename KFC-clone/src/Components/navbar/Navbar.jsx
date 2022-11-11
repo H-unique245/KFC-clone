63f7865251b544132760a09d5827e02bc05bec5c
Rahul Badade <rahulvbadade@gmail.com>
2022-11-11 18:16:11 +0530

    fw18_0737_day-4

--- KFC-clone/src/Components/navbar/Navbar.jsx
+++ KFC-clone/src/Components/navbar/Navbar.jsx
@@ -7,22 +7,22 @@ const Navbar = () => {
     <>
       <div className="nav_main">
         <div className="left_side">
-          <Link to=""> <img className="logo_img" src="/logoeatmore1.png" alt="" /></Link>
+          <Link to="/"> <img className="logo_img" src="/logoeatmore1.png" alt="" /></Link>
 
-          <b><Link className="link" to="">Menu</Link> </b>
-          <b><Link className="link" to="">Deals</Link> </b>
+          <b><Link className="link" to="/menu">Menu</Link> </b>
+          <b><Link className="link" to="/menu">Deals</Link> </b>
           
         </div>
         <div className="right_side">
           <span>
             <img src="/login2.png" alt="" />
           </span>
-          <b><Link className="link" to="">Signup</Link> </b>
+          <b><Link className="link" to="/signup">Signup</Link> </b>
 
          
           <h6 className="cartCountItems">₹ 0.00</h6>
 
-               <Link to=""><img className="cart_img" src="/cart.svg" alt="" /></Link>   
+               <Link to="/cart"><img className="cart_img" src="/cart.svg" alt="" /></Link>   
                  
         </div>
       </div>
