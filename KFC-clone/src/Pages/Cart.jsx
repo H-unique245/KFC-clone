0f7fea7005cc0fe63d7c6a36ed185c90deabcfda
Mohd Monish <97455677+Mohd2002Monish@users.noreply.github.com>
2022-12-23 22:50:29 +0530

    Order Now Funcationality  Added To Cart

--- KFC-clone/src/Pages/Cart.jsx
+++ KFC-clone/src/Pages/Cart.jsx
@@ -1,20 +1,33 @@
 import React, { useEffect, useState } from "react";
 import CartItemComponent from "../Components/CartItemComponent";
-import { Link, useNavigate } from "react-router-dom";
-import { Button } from "../Components/main_button/Button";
+import { Link } from "react-router-dom";
+
 import {
   deleteItem,
   getcartItem,
 } from "../Redux/cartRedux/cart.actions";
 import { useDispatch, useSelector } from "react-redux";
+import {
+  Box,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  Button,
+  Text,
+} from "@chakra-ui/react";
 
 function Cart() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const finalRef = React.useRef(null);
   const { data } = useSelector((store) => store.cart);
   const [products, setProducts] = useState(data);
 
   const dispatch = useDispatch();
-
-  const navigate = useNavigate();
 
   const handleclick = (id) => {
     setTimeout(() => {
@@ -32,13 +45,61 @@ function Cart() {
       <div className="black">
         <b>LET'S ORDER FOR DELIVERY, PICK UP, OR DINE-IN</b>
 
-        <Button style={{fontWeight:"600"}}
-          onClick={() => {
-            navigate("/menu");
-          }}
-        >
-          Order More
-        </Button>
+        <>
+          <Button mt={4} onClick={onOpen}>
+            <Text fontWeight={"bold"}> Order Now</Text>
+          </Button>
+          <Modal
+            finalFocusRef={finalRef}
+            isOpen={isOpen}
+            onClose={onClose}
+          >
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Visit</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Box>
+                  <Button >Button</Button>
+                </Box>
+                <br/>
+                <Box>
+                  <Button >Button</Button>
+                </Box>
+                  <br/>
+                <Box>
+                  <Button >Button</Button>
+                </Box>
+                  <br/>
+                <Box>
+                  <Button >Button</Button>
+                </Box>
+                  <br/>
+                <Box>
+                  <Button >Button</Button>
+                </Box>
+              </ModalBody>
+
+              <ModalFooter>
+                <Link
+                  to={"/deal"}
+                  style={{ marginRight: "10px" }}
+                >
+                  <Button colorScheme="blue">
+                    <Text fontWeight={"bold"}> Deals</Text>
+                  </Button>
+                </Link>
+                <Link to={"/menu"}>
+                  <Button variant="ghost">
+                    <Text fontWeight={"bold"} Menu>
+                      Menu
+                    </Text>
+                  </Button>
+                </Link>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </>
       </div>
       <CartItemComponent
         data={data}
